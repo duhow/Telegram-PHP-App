de6d9a7952183d11f26b04c7286de769b613a1b8
duhow <contacto@duhowpi.net>
2017-05-09 13:57:59 +0200

    Fix spacing.

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 
 $config['telegram'] = [
-	'id'			=> 0,
-	'key'			=> 'YOUR API KEY',
-	'username'		=> 'MyNameBot',
-	'first_name'	=> 'James Bot'
+	'id'            => 0,
+	'key'           => 'YOUR API KEY',
+	'username'      => 'MyNameBot',
+	'first_name'    => 'James Bot'
 ];
 
 $config['creator'] = 0; // Telegram User ID of bot owner
 
 $config['mysql'] = [
-	'host'		=> 'localhost',
-	'username'	=> 'mysql',
-	'password'	=> '',
-	'db'		=> 'telegram',
-	'port'		=> 3306,
-	'prefix'	=> NULL,
-	'charset'	=> NULL
+	'host'      => 'localhost',
+	'username'  => 'mysql',
+	'password'  => '',
+	'db'        => 'telegram',
+	'port'      => 3306,
+	'prefix'    => NULL,
+	'charset'   => NULL
 ];
 
 $config['mysql']['enable'] = TRUE; // Enable MySQL Class / Service.
